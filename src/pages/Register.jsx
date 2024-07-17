a58a343e3c80df423f8d5d406b2b461a47946871
SHiuN26 <justodme26@gmail.com>
2024-07-17 13:45:13 +0800

    loginpage view done

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,7 +1,14 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 
 const Register = () => {
-  return <div>Register</div>;
+  const navigate = useNavigate();
+  return (
+    <>
+      <div>Register</div>
+      <button onClick={() => navigate("/login")}>back to login page</button>
+    </>
+  );
 };
 
 export default Register;
