91dcc6757fe3e67cec07319b67d8d7ae618108b2
SHiuN26 <justodme26@gmail.com>
2024-07-22 03:11:17 +0800

    build

--- src/axiosInsyance.js
+++ src/axiosInsyance.js
@@ -20,4 +20,27 @@ axiosInstance.interceptors.request.use(
   }
 );
 
+axiosInstance.interceptors.response.use(
+  (response) => {
+    return response;
+  },
+  (error) => {
+    console.log("error", error);
+    if (error.response && error.response.status === 401) {
+      // 檢查是否是登入請求
+      const isLoginRequest = error.config.url.includes("/login");
+
+      if (!isLoginRequest) {
+        // 只有在非登入請求的情況下才清除token並重定向
+        localStorage.removeItem("token");
+        localStorage.removeItem("userId");
+        localStorage.removeItem("username");
+        localStorage.removeItem("role");
+        window.location.href = "/login"; // 重定向到登入頁
+      }
+    }
+    return Promise.reject(error);
+  }
+);
+
 export default axiosInstance;
